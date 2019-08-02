878a3673e22ae6f8d41e6f671de9793b0852c4c3
Mimoun Bijjou <mimounbijjou@Mimouns-MacBook-Air.fritz.box>
2019-08-02 22:42:08 +0200

    theme for main page

--- kardio_mapper/settings.py
+++ kardio_mapper/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'kardio_mapper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
